9c3747a9c6bfe10294485abf922783a11e51ef8e
Jordan <jordan.grace@gmail.com>
2020-05-08 14:12:25 +0200

    Initial Git Push -routing -shell -homepageCom

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAhX-F95Sme-Mjurm17Kcfit1r6w5oaJAE",
+    authDomain: "angular-tester-b355e.firebaseapp.com",
+    databaseURL: "https://angular-tester-b355e.firebaseio.com",
+    projectId: "angular-tester-b355e",
+    storageBucket: "angular-tester-b355e.appspot.com",
+    messagingSenderId: "1015640319060",
+    appId: "1:1015640319060:web:dc4d795c8e6eab228bb9c1",
+    measurementId: "G-BM2VDGYBFV"
+  }
 };
